cc2a43e3721521238b3e506cd0af682a46d14cb0
DavidB <david@localhost.localdomain>
2022-06-07 19:06:40 +0100

    logout functionality and initial css

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -11,7 +11,8 @@ const Homepage = () => {
   let accessToken = ""; //hash === "" ? undefined : hash;
   const LOGIN_URL =
     "https://dav-eng-code-testing.eu.auth0.com/authorize?audience=https://productivity-inventory.herokuapp.com/&response_type=token&client_id=Q7pQU8jGRAiOCGEqn55STzPPmevq3rsb&redirect_uri=http://localhost:1234/";
-  //const LOGOUT_URL =    "https://dav-eng-code-testing.eu.auth0.com/v2/logout?client_id=Q7pQU8jGRAiOCGEqn55STzPPmevq3rsb&returnTo=http://localhost:1234/";
+  const LOGOUT_URL =
+    "https://dav-eng-code-testing.eu.auth0.com/v2/logout?client_id=Q7pQU8jGRAiOCGEqn55STzPPmevq3rsb&returnTo=http://localhost:1234/";
   const API_URL = "https://productivity-inventory.herokuapp.com";
   //let API_REQUEST_HEADERS = {};
 
@@ -19,22 +20,11 @@ const Homepage = () => {
   const [apiChanged, setApiChanged] = useState(false);
 
   console.log("Start functional component (Homepage) render...");
-  console.log("check current state of apiRequestHeaders: ", apiRequestHeaders);
+  //console.log("check current state of apiRequestHeaders: ", apiRequestHeaders);
 
   const handleChange = (val) => {
     setApiChanged(val);
   };
-
-  if (hash !== "" && accessToken !== hash) {
-    console.log(
-      "setting request headers in local storage: ",
-      apiRequestHeaders
-    );
-    window.localStorage.setItem(
-      "reqHeaderLocalStorage",
-      JSON.stringify(apiRequestHeaders)
-    );
-  }
 
   let getAccessToken = () => {
     if (accessToken !== hash) {
@@ -42,7 +32,7 @@ const Homepage = () => {
       accessToken = hash;
       let extractedToken = accessToken.replace("#access_token=", "");
       extractedToken = extractedToken.replace(/&.*/, "");
-      console.log("token: ", extractedToken);
+      //console.log("token: ", extractedToken);
       console.log("setting state");
       setApiRequestHeaders({
         Authorization: `Bearer ${extractedToken}`,
@@ -55,29 +45,70 @@ const Homepage = () => {
   useEffect(() => {
     console.log("running effect");
     !(hash === "") ? getAccessToken() : console.log("no!!!"), [];
-    console.log(
-      "check current state of apiRequestHeaders: ",
-      apiRequestHeaders
-    );
+    //console.log("check current state of apiRequestHeaders: ",apiRequestHeaders);
   }, [hash]);
 
   useEffect(() => {
     console.log("getting request headers from local storage...");
     let fromLocalStorage = window.localStorage.getItem("reqHeaderLocalStorage");
     console.log("got: ", fromLocalStorage);
-    if (Object.keys(JSON.parse(fromLocalStorage)).length !== 0) {
+    if (
+      fromLocalStorage !== "" &&
+      Object.keys(JSON.parse(fromLocalStorage)).length !== 0
+    ) {
       setApiRequestHeaders(JSON.parse(fromLocalStorage));
     }
-    console.log("this is what we got: ", apiRequestHeaders);
+    //console.log("this is what we got: ", apiRequestHeaders);
   }, []);
+
+  // useEffect(() => {
+  //   console.log(
+  //     `logging out... hash:${hash},accessToken:${accessToken},apiRequestHeaders:${JSON.stringify(
+  //       apiRequestHeaders
+  //     )},local storage:${window.localStorage.getItem("reqHeaderLocalStorage")}`
+  //   );
+  // }, [apiRequestHeaders]);
+
+  async function logoutFunction(e) {
+    if (Object.keys(apiRequestHeaders).length === 0) {
+      return;
+    }
+    e.preventDefault();
+    accessToken = ""; //clear the accessToken
+    window.localStorage.setItem("reqHeaderLocalStorage", ""); //clear the copy in local storage
+    setApiRequestHeaders({}); //clear the request headers in state
+    console.log("///////////////////1");
+    location.replace("/");
+    console.log("///////////////////2");
+    location.replace(LOGOUT_URL);
+    console.log("///////////////////3");
+  }
+
+  if (hash !== "" && accessToken !== hash) {
+    // console.log(
+    //   "setting request headers in local storage: ",
+    //   apiRequestHeaders
+    // );
+    window.localStorage.setItem(
+      "reqHeaderLocalStorage",
+      JSON.stringify(apiRequestHeaders)
+    );
+    location.replace("/");
+  }
+
   //console.log("current access token: ", accessToken);
-  console.log("request header: ", JSON.stringify(apiRequestHeaders));
-  console.log(Object.keys(apiRequestHeaders).length == 0);
+  //console.log("request header: ", JSON.stringify(apiRequestHeaders));
+  //console.log(Object.keys(apiRequestHeaders).length == 0);
   console.log("about to return page content....");
+
   return (
     <div>
       <a href={LOGIN_URL}>Login</a>
-      {/*<a href={LOGOUT_URL}>Logout</a>*/}
+      {
+        <a href={LOGOUT_URL} onClick={(e) => logoutFunction(e)}>
+          Logout
+        </a>
+      }
       {Object.keys(apiRequestHeaders).length !== 0 &&
       apiRequestHeaders !== null ? (
         <div>
