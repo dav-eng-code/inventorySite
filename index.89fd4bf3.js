20d46f359b463d44e0780aa4963fb6fa159ffbbe
DavidB <david@localhost.localdomain>
2022-06-25 14:16:09 +0100

    Updates

---
+++ index.89fd4bf3.js
@@ -0,0 +1,2 @@
+!function(l){if(console.log("gh pages, l: ",l),"/"===l.search[2]){var n=l.search.slice(1).split("&").map((function(l){return l.replace(/~and~/g,"&")})).join("?");alert(null,null,l.pathname.slice(0,-1)+n+l.hash),console.log(null,null,l.pathname.slice(0,-1)+n+l.hash),window.history.replaceState(null,null,l.pathname.slice(0,-1)+n+l.hash)}}(window.location);
+//# sourceMappingURL=index.89fd4bf3.js.map
