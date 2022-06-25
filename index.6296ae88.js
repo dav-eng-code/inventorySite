873f2961e41a4130d935fa52802be7cfb454d0ca
DavidB <david@localhost.localdomain>
2022-06-25 14:14:33 +0100

    Updates

--- index.6296ae88.js
+++ index.6296ae88.js
@@ -8,12 +8,14 @@
 // When the single page app is loaded further down in this file,
 // the correct url will be waiting in the browser's history for
 // the single page app to route accordingly.
-//alert("running gh-pages script")
-(function(l) {
+alert("running gh-pages script")(function(l) {
+    console.log("gh pages, l: ", l);
     if (l.search[2] === "/") {
         var decoded = l.search.slice(1).split("&").map(function(s) {
             return s.replace(/~and~/g, "&");
         }).join("?");
+        alert(null, null, l.pathname.slice(0, -1) + decoded + l.hash);
+        console.log(null, null, l.pathname.slice(0, -1) + decoded + l.hash);
         window.history.replaceState(null, null, l.pathname.slice(0, -1) + decoded + l.hash);
     }
 })(window.location);
---
+++ index.ab0059fd.js
@@ -0,0 +1,2 @@
+alert("running gh-pages script")((function(l){if(console.log("gh pages, l: ",l),"/"===l.search[2]){var n=l.search.slice(1).split("&").map((function(l){return l.replace(/~and~/g,"&")})).join("?");alert(null,null,l.pathname.slice(0,-1)+n+l.hash),console.log(null,null,l.pathname.slice(0,-1)+n+l.hash),window.history.replaceState(null,null,l.pathname.slice(0,-1)+n+l.hash)}}))(window.location);
+//# sourceMappingURL=index.ab0059fd.js.map
