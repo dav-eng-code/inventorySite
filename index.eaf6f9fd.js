5b505c1b7b0c9008df07cfe7a942ae6548afbdcd
DavidB <david@localhost.localdomain>
2022-06-25 12:33:52 +0100

    Updates

---
+++ index.eaf6f9fd.js
@@ -0,0 +1,2 @@
+console.log("running gh-pages script")((function(n){if("/"===n.search[1]){var a=n.search.slice(1).split("&").map((function(n){return n.replace(/~and~/g,"&")})).join("?");window.history.replaceState(null,null,n.pathname.slice(0,-1)+a+n.hash)}}))(window.location);
+//# sourceMappingURL=index.eaf6f9fd.js.map
