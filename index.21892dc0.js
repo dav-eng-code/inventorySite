e5d7d58f87d08c76b7ca57702e7c43d0bddedc92
DavidB <david@localhost.localdomain>
2022-06-25 13:42:05 +0100

    Updates

---
+++ index.21892dc0.js
@@ -0,0 +1,2 @@
+!function(a){if("/"===a.search[1]){var l=a.search.slice(1).split("&").map((function(a){return a.replace(/~and~/g,"&")})).join("?");alert(null,null,a.pathname.slice(0,-1)+l+a.hash),window.history.replaceState(null,null,a.pathname.slice(0,-1)+l+a.hash)}}(window.location);
+//# sourceMappingURL=index.21892dc0.js.map
